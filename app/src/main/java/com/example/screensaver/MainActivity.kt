20dbc9b83fd56188d38f5d4cee647f029331ee33
youcova428 <y.k.0428.777@gmail.com>
2021-12-09 23:46:41 +0900

    mainActivity -> DreamServiceSetting に変更した。 DreamSerViceはドック、充電中、自ら設定で選択しないと起動しないことが判明した。実機が必要？

--- app/src/main/java/com/example/screensaver/MainActivity.kt
+++ app/src/main/java/com/example/screensaver/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.example.screensaver
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.Preference
 import android.provider.Settings
 import android.service.dreams.DreamService
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : FragmentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val serviceIntent = Intent(this, ScreenSaver::class.java)
-        serviceIntent.action = Settings.ACTION_DREAM_SETTINGS
-        startService(serviceIntent)
+//        val serviceIntent = Intent(this, ScreenSaver::class.java)
+//        serviceIntent.action = Settings.ACTION_DREAM_SETTINGS
+//        startService(serviceIntent)
+
+        val sharedPreferences : SharedPreferences = getSharedPreferences("SharedPreference", Context.MODE_PRIVATE)
+        val switchInteractive = findViewById<Switch>(R.id.switch_service_interactive)
+        switchInteractive.isChecked = sharedPreferences.getBoolean("isInteractive",true)
+        switchInteractive.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("isInteractive",isChecked)
+            sharedPreferences.edit().apply()
+        }
+
+        val switchFullscreen = findViewById<Switch>(R.id.switch_service_fullscreen)
+        switchFullscreen.isChecked = sharedPreferences.getBoolean("isFullScreen",true)
+        switchFullscreen.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("isFullScreen",isChecked)
+            sharedPreferences.edit().apply()
+        }
+
+        val switchScreenBright = findViewById<Switch>(R.id.switch_service_screenbright)
+        switchFullscreen.isChecked = sharedPreferences.getBoolean("isScreenBright",true)
+        switchScreenBright.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("isScreenBright",isChecked)
+            sharedPreferences.edit().apply()
+        }
     }
 }
