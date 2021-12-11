1fefc32d717b03c7c96555f15a376b9410d84b96
youcova428 <y.k.0428.777@gmail.com>
2021-12-11 10:06:55 +0900

    画像読み取り実装 startActivityForResultの非推奨の書き換え。

--- app/src/main/java/com/example/screensaver/MainActivity.kt
+++ app/src/main/java/com/example/screensaver/MainActivity.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.Preference
-import android.provider.Settings
-import android.service.dreams.DreamService
+import android.util.Log
 import android.widget.Button
 import android.widget.Switch
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
 
-class MainActivity : FragmentActivity(){
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,25 +19,26 @@ class MainActivity : FragmentActivity(){
 //        serviceIntent.action = Settings.ACTION_DREAM_SETTINGS
 //        startService(serviceIntent)
 
-        val sharedPreferences : SharedPreferences = getSharedPreferences("SharedPreference", Context.MODE_PRIVATE)
+        val sharedPreferences: SharedPreferences =
+            getSharedPreferences("SharedPreference", Context.MODE_PRIVATE)
         val switchInteractive = findViewById<Switch>(R.id.switch_service_interactive)
-        switchInteractive.isChecked = sharedPreferences.getBoolean("isInteractive",true)
+        switchInteractive.isChecked = sharedPreferences.getBoolean("isInteractive", true)
         switchInteractive.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("isInteractive",isChecked)
+            sharedPreferences.edit().putBoolean("isInteractive", isChecked)
             sharedPreferences.edit().apply()
         }
 
         val switchFullscreen = findViewById<Switch>(R.id.switch_service_fullscreen)
-        switchFullscreen.isChecked = sharedPreferences.getBoolean("isFullScreen",true)
+        switchFullscreen.isChecked = sharedPreferences.getBoolean("isFullScreen", true)
         switchFullscreen.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("isFullScreen",isChecked)
+            sharedPreferences.edit().putBoolean("isFullScreen", isChecked)
             sharedPreferences.edit().apply()
         }
 
         val switchScreenBright = findViewById<Switch>(R.id.switch_service_screenbright)
-        switchFullscreen.isChecked = sharedPreferences.getBoolean("isScreenBright",true)
+        switchFullscreen.isChecked = sharedPreferences.getBoolean("isScreenBright", true)
         switchScreenBright.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("isScreenBright",isChecked)
+            sharedPreferences.edit().putBoolean("isScreenBright", isChecked)
             sharedPreferences.edit().apply()
         }
 
@@ -48,7 +47,13 @@ class MainActivity : FragmentActivity(){
             val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
             intent.addCategory(Intent.CATEGORY_OPENABLE)
             intent.setType("image/*")
-            startActivityForResult(intent,1000)
+            //startActivityForResultから書き換え
+            activityResultLauncher.launch("image/*")
         }
     }
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { url ->
+            Log.d("tag", "uri =${url}")
+        }
 }
