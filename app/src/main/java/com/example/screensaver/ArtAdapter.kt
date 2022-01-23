ecba4e0f0a74fed802b1aa008aaabd6a78a2256d
youcova_from_Mac <helloworld.cova@gmail.com>
2022-01-23 20:29:26 +0900

    StaggeredGridLayoutManagerを修正したもののスクロースすると画像のサイズが変わる。 さらに修正が必要。

--- app/src/main/java/com/example/screensaver/ArtAdapter.kt
+++ app/src/main/java/com/example/screensaver/ArtAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
@@ -18,15 +19,16 @@ class ArtAdapter(var artList: MutableList<Art>) : RecyclerView.Adapter<ArtAdapte
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var imageView: ImageView? = null
-
+        var titleText : TextView? = null
         init {
-            imageView = view.findViewById(R.id.recyclerview_item_image)
+            imageView = view.findViewById(R.id.recyclerview_art_item_image)
+            titleText = view.findViewById(R.id.recyclerview_art_item_title)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recyclerview_image_item, parent, false)
+            .inflate(R.layout.recyclerview_art_item, parent, false)
         return ArtAdapter.ViewHolder(view)
     }
 
@@ -36,6 +38,7 @@ class ArtAdapter(var artList: MutableList<Art>) : RecyclerView.Adapter<ArtAdapte
         holder.imageView!!.setOnClickListener {
             listener.OnArtItemClick(artList[position])
         }
+        holder.titleText!!.text = artList[position].title
     }
 
     override fun getItemCount(): Int {
