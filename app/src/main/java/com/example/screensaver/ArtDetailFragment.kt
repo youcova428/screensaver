82cd0d229aefdae9534794d6156cac8fc8ef0205
youcova428 <y.k.0428.777@gmail.com>
2022-02-21 23:50:02 +0900

    https://qiita.com/u-dai/items/bbb6a51968748e0658e2からbitmapを内部ストレージに保存するメソッドをとりあえず追加した。 ベストは内部のダウンロードフォルダに移動されること。それを目指して実装させていく。

--- app/src/main/java/com/example/screensaver/ArtDetailFragment.kt
+++ app/src/main/java/com/example/screensaver/ArtDetailFragment.kt
@@ -1,18 +1,27 @@
 package com.example.screensaver
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import okhttp3.*
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
 
@@ -23,6 +32,7 @@ class ArtDetailFragment : Fragment() {
 
     private var mImageView: ImageView? = null
     private var mDownloadButton: Button? = null
+    private var mArtDetail: Art? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,16 +62,31 @@ class ArtDetailFragment : Fragment() {
                 handler.post {
                     println(responseText)
                     val type = object : TypeToken<Art>() {}.type
-                    val artDetail: Art = Gson().fromJson(responseText, type)
-                    Glide.with(this@ArtDetailFragment).load(artDetail.primaryImage)
+                    mArtDetail = Gson().fromJson(responseText, type)
+                    Glide.with(this@ArtDetailFragment).load(mArtDetail!!.primaryImage)
                         .into(view.findViewById(R.id.art_detail_image))
                 }
             }
         })
 
         mDownloadButton!!.setOnClickListener {
-            //todo ダウンロードメソッドを追加する
+            GlobalScope.launch(Dispatchers.IO){
+                downloadArtImage(view.context, mArtDetail!!.primaryImage)
+            }
 
+        }
+    }
+
+    //todo ダウンロードbitmapファイルを内部ストレージ保存できたっぽいけど、中身自体を確認できない。
+    private fun downloadArtImage(context: Context, artUri: String) {
+        val bitmap = Glide.with(context).asBitmap().load(artUri).submit().get()
+        val directory = ContextWrapper(context).getDir(
+            "image",
+            Context.MODE_PRIVATE
+        )
+        val file = File(directory, "file_name")
+        FileOutputStream(file).use { stream ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
         }
     }
 
