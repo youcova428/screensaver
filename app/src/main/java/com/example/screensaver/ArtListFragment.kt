1c387f3a8e16e6073e27cd5975f70b487b0817f1
cova <helloworld.cova@gmail.com>
2022-04-23 16:50:27 +0900

    fix: 検索バーをSearchViewに変更。
    ArtList画面にて、検索バーのviewをSearchViewに変更してしたことによって、表示・非表示の問題を解消した。
    mArtImageMutableListデータの持ち方に関して考え、修正する。(Fragment間でどう覚えているか。)
    現状、詳細画面に遷移した場合、検索したリストが前回リストを参照し、そこに加えてしまう不具合がある。

--- app/src/main/java/com/example/screensaver/ArtListFragment.kt
+++ app/src/main/java/com/example/screensaver/ArtListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import android.widget.SearchView
 import android.widget.Toast
 import android.widget.Toolbar
 import androidx.annotation.WorkerThread
@@ -13,17 +14,15 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.ferfalk.simplesearchview.SimpleSearchView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.*
 
-class ArtListFragment : Fragment(), SimpleSearchView.SearchViewListener {
+class ArtListFragment : Fragment() {
 
     lateinit var mView: View
-    var mSearchBarFlag: Boolean = false
-    private var mArtSearchView: SimpleSearchView? = null
-    private val mArtImageMutableList = mutableListOf<Art>()
+    private var mArtSearchView: SearchView? = null
+    private var mArtImageMutableList = mutableListOf<Art>()
     private var mArtImageProgress: ProgressBar? = null
 
     override fun onCreateView(
@@ -42,45 +41,36 @@ class ArtListFragment : Fragment(), SimpleSearchView.SearchViewListener {
         val toolbar = view.findViewById<Toolbar>(R.id.art_list_toolbar)
         val viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
 
-        mArtSearchView?.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-            mArtSearchView?.showSearch()
-        }
-
-        // fixme 検索バーの表示・非表示
-        toolbar.setOnClickListener {
-            if (mArtSearchView?.isSearchOpen == true) {
-                mArtSearchView?.closeSearch()
-            } else {
-                mArtSearchView?.showSearch(true)
-            }
-        }
-
         // 検索バーの設置　
-        mArtSearchView?.setOnQueryTextListener(object : SimpleSearchView.OnQueryTextListener {
+        mArtSearchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
 
-            override fun onQueryTextCleared(): Boolean {
-                return false
-            }
-
             override fun onQueryTextSubmit(query: String?): Boolean {
-                // 引数を持ってsearchMuseumObjectを呼びにいく。
+                // 検索ボタンを押下されたらリストが初期化される。
+                mArtImageMutableList = mutableListOf()
                 query?.let { viewModel.searchMuseumObject(it) }
                 return false
             }
         })
 
         // 画面遷移した後でAPIを叩く
-        viewModel.searchInitialMsmObj("sea")
+        // todo トップページには何を表示させるのか考える
+        if (mArtImageMutableList.isEmpty()) {
+            viewModel.searchInitialMsmObj("sea")
+        }
 
         viewModel.initialMsmObjLiveData.observe(viewLifecycleOwner) {
-            searchResultSet(it)
+            if (mArtImageMutableList.isEmpty()) {
+                searchResultSet(it)
+            }
         }
 
         viewModel.msmObjLiveData.observe(viewLifecycleOwner) {
-            searchResultSet(it)
+            if (mArtImageMutableList.isEmpty()) {
+                searchResultSet(it)
+            }
         }
     }
 
@@ -117,24 +107,6 @@ class ArtListFragment : Fragment(), SimpleSearchView.SearchViewListener {
                 (requireActivity() as MuseumActivity).navigateToArtDetail(art.objectId)
             }
         })
-    }
-
-    override fun onSearchViewClosed() {
-        mSearchBarFlag = false
-        Log.d("SimpleSearchView", "onSearchViewShown")
-    }
-
-    override fun onSearchViewClosedAnimation() {
-        Log.d("SimpleSearchView", "onSearchViewClosedAnimation")
-    }
-
-    override fun onSearchViewShown() {
-        mSearchBarFlag = true
-        Log.d("SimpleSearchView", "onSearchViewShown")
-    }
-
-    override fun onSearchViewShownAnimation() {
-        Log.d("SimpleSearchView", "onSearchViewShownAnimation")
     }
 
     private fun searchResultSet(msmObject: MuseumObjectService.MuseumObject) {
